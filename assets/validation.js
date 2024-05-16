440a485de139bfba3e2692f9fe37cb4721277ef8
Skun01 <truongg9655@gmail.com>
2024-05-16 11:42:55 +0700

    change approach with js

--- assets/validation.js
+++ assets/validation.js
@@ -1,59 +1,58 @@
-const haveFormError = false;
-
-function validation(formSelector, formMessageSelector, objectSet, addMessage){
-    const formElem = document.querySelector(formSelector);
-    let haveError = false;
-    objectSet.forEach(inputObject=>{
-        const inputElem = formElem.querySelector(inputObject.selector);
-        const formMessageElem = inputElem.parentElement.querySelector(formMessageSelector);
-        console.log(inputElem);
-        console.log(formMessageElem);
-        inputObject.rules.forEach(rule=>{
-            runningRule(inputElem, formMessageElem, rule);
+function validator(objForm){
+    const formElem = document.querySelector(objForm.formSelector);
+    objForm.setRules.forEach(inputObj=>{
+        const inputElem = document.querySelector(inputObj.inputSelector);
+        inputObj.inputElement = inputElem;
+        const parentElem = inputElem.parentElement;
+        const errorElem = parentElem.querySelector(objForm.errorSelector);
+        inputElem.addEventListener('blur', e=>{
+            const messageList = inputObj.rules;
+            for(message in messageList){
+                if(message){
+                    errorElem.textContent = message;
+                    inputElem.classList.add('invalid');
+                    break;
+                }
+            }
+        });
+        inputElem.addEventListener('input', ()=>{
+            errorElem.textContent = '';
+            inputElem.classList.remove('invalid');
         });
     });
 }
 
-
-function runningRule(inputElem, formMessageElem, rule){
-    switch(rule){
-        case 'required': required(inputElem, formMessageElem, addMessage); break;
-        case 'checkEmail': checkEmail(inputElem, formMessageElem, addMessage); break;
-        case 'checkPhoneNumber': checkPhoneNumber(inputElem, formMessageElem, addMessage); break;
-        case 'checkPassword': checkPassword(inputElem, formMessageElem, addMessage); break;
-        case 'confirmPassword': confirmPassword(inputElem, inputObject.selectorCheck, formMessageElem, addMessage);
-    }
+validator.required = function (inputElement, message){
+    return inputElement.value ? undefined : message;
 }
 
-function addingMessage(inputElem, formMessageElem, message, condition){
-    console.log(condition);
-    if(!condition){
-        formMessageElem.textContent = message;
-        addMessage = true;
-    }else if(condition && addMessage){
-        formMessageElem.textContent = '';
-        addMessage = false;
-    }
-    haveFormError += addMessage;
-}
-
-function required(inputElem, formMessageElem, addMessage){
-    inputElem.addEventListener('blur', ()=>addingMessage(inputElem, formMessageElem, '*Please enter in this text box', inputElem.value, addMessage));
-}
-
-function checkEmail(inputElem, formMessageElem, addMessage){
-    const regex = /^[^\s@]+@[^\s@]+(\.[^\s@])
-    addingMessage(inputElem, formMessageElem, '*This field must be email form', inputElem.value.length === 0 || regex.test(inputElem.value), addMessage);
-}
-
-function checkPhoneNumber(inputElem, formMessageElem, addMessage){
+validator.isEmail = function(inputElement){
 
 }
 
-function checkPassword(inputElem, formMessageElem, addMessage){
+validator.isPhoneNumber = function(inputElement){
 
 }
 
-function confirmPassword(inputElem, formMessageElem, addMessage){
+validator.checkPasswordLength = function(inputElement, min){
 
 }
+
+validator.checkPasswordSecure = function(inputElement){
+
+}
+
+validator.confirmPassword = function(inputElement, checkSelector){
+
+}
+const testing ={
+    name : 'hello',
+    run: test(this.age)
+}
+
+function test(age){
+    console.log(age);
+    return age;
+}
+testing.age = 20;
+console.log(run);
